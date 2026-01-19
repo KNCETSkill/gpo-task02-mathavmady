0b3f448d67a6a67a2d9084cb8275f838001f0498
MATHAVAN V <mathavhocker@gmail.com>
2026-01-19 15:35:58 +0530

    Implement attendance status evaluation logic

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -1,1 +1,15 @@
-
+class AttendanceStatusEvaluator{
+  public static void main(String a[]){
+    Scanner sc =new Scanner(System.in);
+    int attendance=sc.nextInt();
+    if(attendance>85){
+      System.out.println("Excellent");
+    }
+    else if(attendance>60 && attendance<=80){
+       System.out.println("Satisfactory");
+    }
+    else if(attendance<60){
+       System.out.println("Poor");
+    }
+  }
+}
